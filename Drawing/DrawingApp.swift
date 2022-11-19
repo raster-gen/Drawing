96772d50124d76e58bb57833894c52544ff4a330
raster-gen <rasterroot@gmail.com>
2022-11-19 12:47:34 +0000

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Gennady Raster on 19.11.22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
